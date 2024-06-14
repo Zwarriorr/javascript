81cff983aab55bfcc7d81f8872b92101d15bd358
ZwarriorX <ZabianKluson@Gmail.com>
2024-06-14 18:41:28 +0200

    hoi

---
+++ script.js
@@ -0,0 +1,49 @@
+var firstName = "1";
+const secondName = "2";
+let lastName = "3";
+
+const num1 = "4";
+const num2 = "9";
+const num3 = "14";
+
+console.log("")//print//
+
+function calc(number1, number2){
+    console.log(number1 * 5)
+    console.log(number2 * 5)
+}
+
+calc(6, 6)
+
+function actualcalc(number1, number2){
+
+}
+
+actualcalc()
+
+
+
+
+const rand = 100;
+const burger = 35;
+const koeldrank = 15;
+
+function telTotaal(){
+    totaal = burger + koeldrank
+    return total;
+}
+const Totaal = telTotaal()
+console.log(Totaal)
+
+finaalTotaal = 3
+
+if(finaalTotaal > 0){
+    console.log("Jy kry kleingeld")
+}else{
+    console.log("Geen Kleingeld")
+}
+
+
+
+console.log(typeof(rand))
+
