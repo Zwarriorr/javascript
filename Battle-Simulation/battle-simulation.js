b81aa138e7d224815d05fc7489ac5d45bd70c993
ZwarriorX <ZabianKluson@Gmail.com>
2024-06-15 22:32:00 +0200

    new

---
+++ Battle-Simulation/battle-simulation.js
@@ -0,0 +1,7 @@
+if (evading == true){
+    console.log("The Attacker Tried to kick you but You, evaded he took " + evade + " damage")
+    hpB = hpB - evade
+    console.log("Your HP is " + hpA)
+    console.log("His HP is " + hpB)
+    evading = false
+}
